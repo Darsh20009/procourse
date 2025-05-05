68a96cc18a5f9fb928f903b666ea7e293b906108
Darsh20009 <youssefdarwish20009@gmail.com>
2025-05-05 21:57:07 +0000

    Restored to '2f03c4b9b463873c6c3707eecf11858fedd13630'
    Replit-Restored-To: 2f03c4b9b463873c6c3707eecf11858fedd13630

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -4,14 +4,11 @@ import App from "./App";
 import "./index.css";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { queryClient } from "./lib/queryClient";
-import { AuthProvider } from "./context/AuthContext";
 
 createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
-      <AuthProvider>
-        <App />
-      </AuthProvider>
+      <App />
     </QueryClientProvider>
   </React.StrictMode>
 );
